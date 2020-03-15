ca349d9aef525c86d5bb552231dc7ea5e9d7196a
Meowth Perseus <mperseus@at-e3.21-school.ru>
2020-03-15 05:08:03 +0300

    vector and color parsing error handling improved

--- rtv1_read_scene_6.c
+++ rtv1_read_scene_6.c
@@ -6,7 +6,7 @@
 /*   By: mperseus <mperseus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 07:30:13 by mperseus          #+#    #+#             */
-/*   Updated: 2020/03/15 04:04:14 by mperseus         ###   ########.fr       */
+/*   Updated: 2020/03/15 05:06:30 by mperseus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ t_vector	parse_vector(char *value)
 {
 	int			i;
 	char		*tmp;
+	char		*value_start;
 	t_vector	vector;
 
+	value_start = value;
 	tmp = ft_strnew(10);
-	i = ft_strindex(',', value);
+	if ((i = ft_strindex(',', value)) < 0)
+		put_error_wrong_scene_data(value_start, "missing ','");
 	tmp = ft_strncat(tmp, value, i);
 	vector.x = ft_atoi(tmp);
 	ft_bzero(tmp, 10);
 	while (i-- >= 0)
 		value++;
-	i = ft_strindex(',', value);
+	if ((i = ft_strindex(',', value)) < 0)
+		put_error_wrong_scene_data(value_start, "missing ','");
 	tmp = ft_strncat(tmp, value, i);
 	vector.y = ft_atoi(tmp);
 	ft_bzero(tmp, 10);
@@ -45,13 +49,15 @@ t_color		parse_color(char *value)
 
 	value_start = value;
 	tmp = ft_strnew(10);
-	i = ft_strindex(',', value);
+	if ((i = ft_strindex(',', value)) < 0)
+		put_error_wrong_scene_data(value_start, "missing ','");
 	tmp = ft_strncat(tmp, value, i);
 	color.r = check_and_get_int_value(tmp);
 	ft_bzero(tmp, 10);
 	while (i-- >= 0)
 		value++;
-	i = ft_strindex(',', value);
+	if ((i = ft_strindex(',', value)) < 0)
+		put_error_wrong_scene_data(value_start, "missing ','");
 	tmp = ft_strncat(tmp, value, i);
 	color.g = check_and_get_int_value(tmp);
 	ft_bzero(tmp, 10);
